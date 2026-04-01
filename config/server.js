45b6de6a0ad8d12db1c1836e2b3883d40c06e6b3
Praveen-singh-kushwah <praveensingh7990@gmail.com>
2026-04-01 17:44:08 +0530

    small change

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: "https://cms.thynkwise.co.in/",
-  proxy: { koa: true },
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
