a9b9ec09dd7c5d4831a313d074de5fe0ff11e073
Praveen-singh-kushwah <praveensingh7990@gmail.com>
2026-04-14 18:12:29 +0530

    remove the individual partner components from frontend

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://cms.thynkwise.co.in/",
+  // url: "https://cms.thynkwise.co.in/",
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
